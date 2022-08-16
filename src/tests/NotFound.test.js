ac6a35e06a83eeca3a497117547c9aa7a5778219
Camila Wolter <camila0wolter@gmail.com>
2022-08-16 18:03:50 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testanto o componente NotFound.js', () => {
+  test('Testando se a página possui um heading', () => {
+    renderWithRouter(<NotFound />);
+
+    const msgNotFound = screen.getByRole('heading', { level: 2,
+      name: 'Page requested not found Crying emoji' });
+    expect(msgNotFound).toBeInTheDocument();
+  });
+  test('Testamdo se a página renderiza uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgPikachu = screen.getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(imgPikachu.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
