cb0a3525b230ce9fdc36085b2fbe6b4ac9af0c6b
Camila Wolter <camila0wolter@gmail.com>
2022-08-16 17:40:34 -0300

    requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testanto o componente FavoritePokemons.js', () => {
+  test('Testando se a mensagem é exibida corretamente sem nenhum pokémon', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const msg = screen.getByText(/No favorite pokemon found/i);
+    expect(msg).toBeInTheDocument();
+  });
+});
