91a719504501b2c1b9c16ba68e8136b447560356
Camila Wolter <camila0wolter@gmail.com>
2022-08-16 17:14:22 -0300

    requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('teste', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Testanto o componente About', () => {
+  test('Testando se possuem um heading', () => {
+    renderWithRouter(<About />);
+
+    const headingAbout = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(headingAbout).toBeInTheDocument();
+  });
+  test('Testanto se a página tem dois parágrafos', () => {
+    renderWithRouter(<About />);
+
+    const paragraUm = screen.getByText(/This application simulates a Pokédex/i);
+    expect(paragraUm).toBeInTheDocument();
+
+    const paragraDois = screen.getByText(/One can filter Pokémons by type/i);
+    expect(paragraDois).toBeInTheDocument();
+  });
+  test('Testando se possui uma imagem', () => {
+    renderWithRouter(<About />);
+
+    const imgPokedex = screen.getByRole('img', { name: 'Pokédex' });
+    expect(imgPokedex.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
