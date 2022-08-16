a0a6c328f7aad7e9751fb5399c9224629cebdf48
Camila Wolter <camila0wolter@gmail.com>
2022-08-16 19:12:12 -0300

    requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente Pokedex.js', () => {
+  test('Testando se a página contém um heading', () => {
+    renderWithRouter(<App />);
+
+    const msgHeading = screen.getByRole('heading', { level: 2,
+      name: 'Encountered pokémons' });
+    expect(msgHeading).toBeInTheDocument();
+  });
+  test('Testando ao clicar no botão "Próximo pokémon" é exibido cada pokémon', () => {
+    renderWithRouter(<App />);
+
+    const buttonNext = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(buttonNext).toBeInTheDocument();
+
+    userEvent.click(buttonNext);
+    const charmanderPokemon = screen.getByText(/Charmander/i);
+    expect(charmanderPokemon).toBeInTheDocument();
+
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    const pikachuPokemon = screen.getByText(/Pikachu/i);
+    expect(pikachuPokemon).toBeInTheDocument();
+  });
+  test('Testando os botões de filtro e o botão de All', () => {
+    renderWithRouter(<App />);
+
+    const allButtonsFilter = 7;
+    const buttonsFilter = screen.getAllByTestId(/pokemon-type-button/i);
+    expect(buttonsFilter).toHaveLength(allButtonsFilter);
+
+    const fireButton = screen.getByRole('button', { name: 'Fire' });
+    const buttonAll = screen.getByRole('button', { name: 'All' });
+
+    expect(fireButton).toBeInTheDocument();
+    expect(buttonAll).toBeInTheDocument();
+
+    userEvent.click(fireButton);
+    const charmanderPokemon = screen.getByText(/Charmander/i);
+    expect(charmanderPokemon).toBeInTheDocument();
+    expect(buttonAll).toBeInTheDocument();
+
+    userEvent.click(buttonAll);
+    const pikachuPokemon = screen.getByText(/Pikachu/i);
+    expect(pikachuPokemon).toBeInTheDocument();
+  });
+});
